48127abcb48e1b4fa72b2dea79463ada859dca3c
Foysal Hossain <foysalhossain772@gmail.com>
2023-05-05 02:32:04 +0600

    ragings added

--- src/components/Recipies/Recipies.jsx
+++ src/components/Recipies/Recipies.jsx
@@ -5,11 +5,21 @@ const Recipies = ({ item }) => {
     return (
         <div className='border-2 p-5'>
             <h1 className='font-bold my-4 text-center'>{item.recipe_name}</h1>
+            <h1 className='font-bold my-4 '>Ingredients</h1>
             <div>
                 {
                     item.ingredients.map((ingredient, index) => <p key={index}>{index + 1}. {ingredient}</p>)
                 }
             </div>
+            <br />
+            <h1 className='font-bold my-4 '>Cooking</h1>
+            <div>
+                {
+                    item.cooking.map((cooking, index) => <p key={index}>{index + 1}. {cooking}</p>)
+                }
+            </div>
+
+            <h3 className='my-4'><span className='font-semibold'>Rating:</span> {item.rating}</h3>
         </div>
     );
 };
